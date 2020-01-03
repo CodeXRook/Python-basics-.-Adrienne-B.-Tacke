98bc65b32a606776ccf7340d012504ced5a5c103
CodeXRook <lacekitfly@gmail.com>
2020-01-02 22:26:04 -0500

    vars can't start with num

--- variables/var.py
+++ variables/var.py
@@ -4,5 +4,11 @@ favorite_number = 3
 type(favorite_number)
 
 favorite_number = "3"
-#type of int
+#type of class string
 type(favorite_number)
+
+#Cannot start variables with number
+#100_days_of_code = 100
+
+#correct way 
+one_100_days_code = 100
