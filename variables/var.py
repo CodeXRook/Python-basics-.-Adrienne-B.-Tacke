8816db2193f64e8432c518e678223d560505d489
CodeXRook <lacekitfly@gmail.com>
2020-01-02 23:42:55 -0500

    got second test

--- variables/var.py
+++ variables/var.py
@@ -33,3 +33,4 @@ print("\"Coding\" is \"cool\" in Python!")
 
 #print quotes with quotes
 print("\"Coding is a superpower! You can do so many cool things with your imagination and code.\"")
+print("\"Coding is a superpower! You can do so many cool things with your imgination and code.\"")
